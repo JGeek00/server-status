3013362b991562fcef4fb3a67bedca0684828a8e
Juan Gilsanz Polo <juangilsanz@gmail.com>
2024-03-25 14:26:29 +0100

    Removed cocoapods and added sentry on swift package manager

--- Pods/Sentry/Sources/Sentry/include/SentryPrivate.h
+++
@@ -1,9 +0,0 @@
-// Sentry internal headers that are needed for swift code
-
-#import "SentryBaggage.h"
-#import "SentryBaseIntegration.h"
-#import "SentryDispatchQueueWrapper.h"
-#import "SentryRandom.h"
-#import "SentrySdkInfo.h"
-#import "SentryStatsdClient.h"
-#import "SentryTime.h"
