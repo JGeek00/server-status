daa124d8746c56b79b2888739e1c959158ed1f4a
Juan Gilsanz Polo <juangilsanz@gmail.com>
2024-05-13 20:51:03 +0200

    Added settings button no instances tablet

--- ServerStatus/Views/NoInstances/NoInstancesView.swift
+++ ServerStatus/Views/NoInstances/NoInstancesView.swift
@@ -4,6 +4,9 @@ struct NoInstancesView: View {
     @StateObject var instanceFormModel = InstanceFormViewModel()
     
     @Environment(\.horizontalSizeClass) var horizontalSizeClass
+    @Environment(\.colorScheme) var scheme
+    
+    @State var showSettingsSheet = false
     
     var body: some View {
         VStack {
@@ -14,24 +17,44 @@ struct NoInstancesView: View {
             Text("Create a new instance to begin.")
                 .multilineTextAlignment(.center)
             Spacer().frame(height: 50)
-            Button {
-                instanceFormModel.reset()
-                instanceFormModel.modalOpen.toggle()
-            } label: {
-                Label {
-                    Text("Add instance")
-                        .foregroundColor(.white)
-                } icon: {
-                    Image(systemName: "plus")
-                        .foregroundColor(.white)
-                }
+            HStack {
+                Button {
+                    instanceFormModel.reset()
+                    instanceFormModel.modalOpen.toggle()
+                } label: {
+                    Label {
+                        Text("Add instance")
+                            .foregroundColor(.white)
+                    } icon: {
+                        Image(systemName: "plus")
+                            .foregroundColor(.white)
+                    }
 
+                }
+                .buttonStyle(.borderedProminent)
+                .cornerRadius(50)
+                .padding(10)
+                Spacer()
+                    .frame(width: 20)
+                if horizontalSizeClass == .regular {
+                    Button {
+                        showSettingsSheet.toggle()
+                    } label: {
+                        Label {
+                            Text("Settings")
+                                .foregroundColor(.white)
+                        } icon: {
+                            Image(systemName: "gear")
+                                .foregroundColor(.white)
+                        }
+
+                    }
+                    .buttonStyle(.borderedProminent)
+                    .cornerRadius(50)
+                    .padding(10)
+                }
             }
-            .buttonStyle(.borderedProminent)
-            .cornerRadius(50)
-            .padding(10)
         }
-        .navigationTitle("Instances")
         .padding()
         .sheet(isPresented: $instanceFormModel.modalOpen, content: {
             InstanceFormView(instanceFormModel: instanceFormModel)
@@ -40,13 +63,18 @@ struct NoInstancesView: View {
             ToolbarItem(placement: .secondaryAction) {
                 if horizontalSizeClass == .regular {
                     Button {
-                        
+                        showSettingsSheet.toggle()
                     } label: {
                         Image(systemName: "gear")
                     }
                 }
             }
         }
+        .sheet(isPresented: $showSettingsSheet, content: {
+            SettingsView(scheme: scheme) {
+                showSettingsSheet.toggle()
+            }
+        })
     }
 }
 
